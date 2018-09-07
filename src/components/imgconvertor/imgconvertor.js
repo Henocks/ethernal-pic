8341a462b0e6cde6226bcfc1ba2d7b4ad97bc2cf
Henocks <hr4574@naver.com>
2018-09-07 17:34:59 +0900

    Added ;s

--- src/components/imgconvertor/imgconvertor.js
+++ src/components/imgconvertor/imgconvertor.js
@@ -2,22 +2,22 @@ import React from 'react';
 
 class ImageConvertor extends React.Component {
   state = {
-    imgResult : ''
+    imgResult : null
   }
 
   encodeBase64ImageFile = (image) => {
     return new Promise((resolve, reject) => {
-      let reader = new FileReader()
+      let reader = new FileReader();
       // convert the file to base64 text
-      reader.readAsDataURL(image)
+      reader.readAsDataURL(image);
       // on reader load somthing...
       reader.onload = (event) => {
-        resolve(event.target.result)
+        resolve(event.target.result);
       }
       reader.onerror = (error) => {
         reject(error)
       }
-    })
+    });
   }
 
   onFileChange = (event) => {
