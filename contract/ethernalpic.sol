ba9df1567113040e0e5e561baa8d7581bd67eeab
Henocks <hr4574@naver.com>
2018-09-10 17:36:49 +0900

    new function : licensed pics

--- contract/ethernalpic.sol
+++ contract/ethernalpic.sol
@@ -3,37 +3,58 @@ pragma solidity ^0.4.24;
 contract EthernalPic {
     
     event createdPic(address sender, uint256 counter);
+
+    struct Picture {
+        string rawPictureData;
+        string licenseData;
+        address uploader;
+        address owner;
+        uint256 value;
+        uint256 timestamp;
+    }
     
-    mapping (uint256 => string) public pictures;
+    mapping (uint256 => Picture) public pictures;
     uint256 public counter;
     
     constructor()
-        public
+    public
     {
         counter = 0;
     }
     
-    function createPic(string picture)
-        public
+    function createPic(string picture, address _owner)
+    public
     {
-        pictures[counter] = picture;
+        pictures[counter] = Picture(picture, "null", msg.sender, _owner, 0, block.timestamp);
         emit createdPic(msg.sender, counter);
         counter++;
     }
 
-    function createLicensedPic(string picture, string licenseData)
+    function createLicensedPic(string picture, string licenseData, address _owner)
     public
     payable
     {
-        
+        pictures[counter] = Picture(picture, licenseData, msg.sender, _owner, 0, block.timestamp);
+        emit createdPic(msg.sender, counter);
+        counter++;
     }
     
+
+
     function findPic(uint256 _counter)
-        public
-        view
-        returns(string result)
+    public
+    view
+    returns(string result)
     {
-        result = pictures[_counter];
+        //result = pictures[_counter];
+    }
+
+    function findLicensedPic(uint256 _counter)
+    public
+    view
+    returns(string result, string licenseData)
+    {
+
     }
 
 
